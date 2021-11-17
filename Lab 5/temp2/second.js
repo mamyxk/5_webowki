63fd112718f18bfec5daafcca890a278229aae81
Kamil Haniszewski <kamil.haniszewski@systemeg.internal>
2021-11-17 17:15:10 +0100

    dodaney

---
+++ Lab 5/temp2/second.js
@@ -0,0 +1,11 @@
+// var xx = parseInt(x);
+// var yy = parseInt(y);
+// console.log("Fun run  ", xx, "  ", yy);
+// if (xx > yy) {
+//     a = parseInt(x);
+//     b = parseInt(y);
+//     console.log(a, "  ", b);
+// }
+
+// var res = Math.sqrt(a * a - b * b);
+// document.getElementById("triangle_res").innerHTML = res;
--- Lab 5/tymczasowy/main.js
+++ Lab 5/tymczasowy/main.js
@@ -1,12 +1,12 @@
-document.getElementById("myButton").addEventListener("click", function () {
-  var iframeDocument = window.frames["myiframe"].document;
-  iframeDocument.open();
-  iframeDocument.writeln("<p style='color: red;'>Hello, world!</p>");
-  iframeDocument.close();
+document.getElementById("myButton").addEventListener("click", function() {
+    var iframeDocument = window.frames["myiframe"].document;
+    iframeDocument.open();
+    iframeDocument.writeln("<p style='color: red;'>Hello, world!</p>");
+    iframeDocument.close();
 });
 
-document.getElementById("button1").addEventListener("click", function () {
-  var textarea = getElementById("textarea1");
-  var text = window.prompt("Podaj tekst który ma być wypisany");
-  
+document.getElementById("button1").addEventListener("click", function() {
+    var textarea = getElementById("textarea1");
+    var text = window.prompt("Podaj tekst który ma być wypisany");
+
 })
