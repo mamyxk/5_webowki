e984397b23f425fa7c07e519eec8d65d6df160ff
Kamil <k.haniszewski97@gmail.com>
2022-01-12 18:37:11 +0100

    db_scripts

--- lab_9/delete_database.php
+++ lab_9/delete_database.php
@@ -2,15 +2,13 @@
 
 require('db_config.php');
 
-$query_create = "create database ".$db_name.";";
+$query_drop = "drop database ".$db_name.";";
 
 $conn = new mysqli($db_host, $db_user, $db_pass);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  $conn ->query($query_create);
-
-
+  $conn ->query($query_drop);
 
 ?>
--- lab_9/login_form.php
+++ lab_9/login_form.php
@@ -4,20 +4,16 @@ require('db_config.php');
 
 $conn = new mysqli($db_host, $db_user, $db_pass);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-die();
-
-
 if(isset($_SESSION['logged'])){
     //Zalogowany
 }else{
     //Niezalogowany
     if(isset($_POST['uname'])){
-        $connection = 
+        $connection = new mysqli($db_host, $db_user, $db_pass);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+          echo "Connected successfully";
     }
 }
 
