af73f232828fba15d533a7e60d70eeb610dcc8ca
Kamil <k.haniszewski97@gmail.com>
2022-01-12 17:43:33 +0100

    db_conf

---
+++ lab_9/create_database.php
@@ -0,0 +1,7 @@
+<?php
+
+$db_name = "sys_webowe";
+$query_create = "create database ".$db_name.";";
+
+
+?>
--- lab_9/db_config.php
+++ lab_9/db_config.php
@@ -1,3 +1,6 @@
 <?php
+$db_host = "";
+$db_user = "kamil";
+$db_pass = "N0t@Kam!l";
 
 ?>
