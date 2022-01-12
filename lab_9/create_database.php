9100c12e1d66bfdb9e41be3f1681e3a384d97846
Kamil <k.haniszewski97@gmail.com>
2022-01-12 18:42:06 +0100

    create_db

--- lab_9/create_database.php
+++ lab_9/create_database.php
@@ -2,14 +2,23 @@
 
 require('db_config.php');
 
-$query_create = "create database ".$db_name.";";
+$query_create_db = "create database ".$db_name.";";
+
+$query_create_table_users = "create table Users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    login VARCHAR(20) NOT NULL,
+    passowrd VARCHAR(128) NOT NULL,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(40) NOT NULL,
+    reg_date TIMESTAMP DEFAUL CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
 
 $conn = new mysqli($db_host, $db_user, $db_pass);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-  $conn ->query($query_create);
+  $conn ->query($query_create_db);
 
 
 
