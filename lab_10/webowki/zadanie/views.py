efff19f8905cfbb557b9dbbb5d91845acd784314
Kamil-Lontkowski <kamillont14@gmail.com>
2022-01-19 17:09:40 +0100

    Working first page

--- lab_10/webowki/zadanie/views.py
+++ lab_10/webowki/zadanie/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def hello(request):
-    return render(request, 'hello.html')
+def home(request):
+    return render(request, "zadanie/hello.html")
